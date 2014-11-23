89d6c7693b95809da77041e652e26968d0cf6ed6
Jonathan Carnie <jono.carnie@gmail.com>
2014-11-23 12:43:47 +1100

    Initial Commit

---
+++ JCRSSReaderTests/JCRSSReaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  JCRSSReaderTests.swift
+//  JCRSSReaderTests
+//
+//  Created by Jonathan Carnie on 23/11/2014.
+//  Copyright (c) 2014 Jonathan Carnie. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class JCRSSReaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
